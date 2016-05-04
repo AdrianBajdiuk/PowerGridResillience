7debbcc51c049080171f2063d224ccf3f953dc5d
AdrianBajdiuk <adrian.bajdiuk@gmail.com>
2016-05-05 00:16:17 +0200

    logs

--- main.py
+++ main.py
@@ -20,16 +20,25 @@ from Helper import copyCase
 from PowerSim.ResiliencyMethods import MethodBase, ESPEdge, ESPVertex, RandomEdge, RandomVertex
 import logging
 
-logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
-rootLogger = logging.getLogger()
-
-fileHandler = logging.FileHandler("{0}/{1}.log".format("/logs", "sim"))
-fileHandler.setFormatter(logFormatter)
-rootLogger.addHandler(fileHandler)
-
-consoleHandler = logging.StreamHandler()
-consoleHandler.setFormatter(logFormatter)
-rootLogger.addHandler(consoleHandler)
+fileLogPath = "sim.log"
+if not os.path.exists(fileLogPath):
+    flags = os.O_CREAT | os.O_EXCL | os.O_WRONLY
+    os.open(fileLogPath, flags)
+# set up logging to file - see previous section for more details
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+                    datefmt='%m-%d %H:%M',
+                    filename=fileLogPath,
+                    filemode='w')
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
 
 def main():
     parser = OptionParser()
